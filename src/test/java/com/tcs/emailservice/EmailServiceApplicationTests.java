4ad112b0fa909ec4c3cb0f016b8b5bff64001f33
Eureka server <kvineet881@gmail.com>
2020-10-23 23:47:10 +0530

    email service is commited

---
+++ src/test/java/com/tcs/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
